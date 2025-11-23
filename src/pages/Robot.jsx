591c8acbe6044f1e46cc7bf59a5de7fb35557cba
ShravanMukkawar <mukkawarshravan04@gmail.com>
2025-11-23 20:08:38 +0530

    fixed mobile view

--- src/pages/Robot.jsx
+++ src/pages/Robot.jsx
@@ -30,121 +30,31 @@ const Robot = () => {
 
     const interval = setInterval(() => {
       hideSplineBranding();
-      if (splineViewer && splineViewer.shadowRoot) {
-        const logoExists = splineViewer.shadowRoot.querySelector(
-          'a[href*="spline"], [class*="logo"], [id*="logo"]'
-        );
-        if (!logoExists) clearInterval(interval);
-      }
     }, 100);
 
-    const observer = new MutationObserver(hideSplineBranding);
-    observer.observe(splineViewer, { childList: true, subtree: true });
-
-    // Mobile touch events
-    const getCanvas = () =>
-      splineViewer?.shadowRoot?.querySelector("canvas");
-
-    let isTouchActive = false;
-
-    const handleTouchStart = (e) => {
-      const touch = e.touches[0] || e.changedTouches?.[0];
-      const canvas = getCanvas();
-      if (!touch || !canvas) return;
-
-      isTouchActive = true;
-
-      canvas.dispatchEvent(
-        new PointerEvent("pointerdown", {
-          bubbles: true,
-          clientX: touch.clientX,
-          clientY: touch.clientY,
-          pointerType: "touch",
-        })
-      );
-    };
-
-    const handleTouchMove = (e) => {
-      if (!isTouchActive) return;
-      const touch = e.touches[0] || e.changedTouches?.[0];
-      const canvas = getCanvas();
-      if (!touch || !canvas) return;
-
-      canvas.dispatchEvent(
-        new PointerEvent("pointermove", {
-          bubbles: true,
-          clientX: touch.clientX,
-          clientY: touch.clientY,
-          pointerType: "touch",
-        })
-      );
-    };
-
-    const handleTouchEnd = () => {
-      const canvas = getCanvas();
-      if (!canvas) return;
-
-      isTouchActive = false;
-
-      canvas.dispatchEvent(
-        new PointerEvent("pointerup", {
-          bubbles: true,
-          pointerType: "touch",
-        })
-      );
-    };
-
-    if (splineViewer) {
-      splineViewer.addEventListener("touchstart", handleTouchStart, {
-        passive: true,
-      });
-      splineViewer.addEventListener("touchmove", handleTouchMove, {
-        passive: true,
-      });
-      splineViewer.addEventListener("touchend", handleTouchEnd, {
-        passive: true,
-      });
-      splineViewer.addEventListener("touchcancel", handleTouchEnd, {
-        passive: true,
-      });
-    }
-
-    return () => {
-      clearInterval(interval);
-      observer.disconnect();
-
-      if (splineViewer) {
-        splineViewer.removeEventListener("touchstart", handleTouchStart);
-        splineViewer.removeEventListener("touchmove", handleTouchMove);
-        splineViewer.removeEventListener("touchend", handleTouchEnd);
-        splineViewer.removeEventListener("touchcancel", handleTouchEnd);
-      }
-    };
+    return () => clearInterval(interval);
   }, []);
-return (
-  <div className="relative w-full h-full">
 
-    {/* TOP-LEFT TEXT */}
-    <div className="absolute top-10 left-10 z-20">
-      <h1
-        className="special-font
-          text-4xl md:text-5xl
-          leading-[1.1]
-          text-left
-          bg-gradient-to-r from-[#4795E4] to-[#3ABAD7]
-          bg-clip-text text-transparent uppercase"
-      >
-        Investment <br />
-        Banking <br />
-        House
-      </h1>
-      <p className="mt-3 text-lg text-left text-[#030303]">
-        Your trusted AI Partner in Finance.
-      </p>
-    </div>
+  return (
+    <div className="robot">
+      {/* TEXT */}
+      <div className="robot-text">
+        {/* Desktop & tablet version */}
+        <h1 className="special-font robot-title desktop-text">
+          Investment <br />
+          Banking <br />
+          House
+        </h1>
 
-    {/* ROBOT (stays EXACT same position) */}
-    <div className="robot-wrapper">
+        {/* Mobile short version */}
+        <h1 className="special-font robot-title mobile-text">
+          IBH
+        </h1>
+
+        <p className="robot-subtext">Your trusted AI Partner in Finance.</p>
+      </div>
+
+      {/* ROBOT */}
       <div className="robot-container">
         <spline-viewer
           ref={viewerRef}
@@ -152,9 +62,7 @@ return (
         ></spline-viewer>
       </div>
     </div>
-
-  </div>
-);
+  );
 };
 
 export default Robot;
